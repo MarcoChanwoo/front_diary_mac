09a5c5cbeb95f63e861ccc13b5c46e960e35bcad
Marco <cwhong8087@gmail.com>
2023-09-05 22:36:47 +0900

    11th commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,12 @@ const Home = () => {
     return (
         <div>
             <Editor
-                onSubmit={() => {
-                    alert("완료 버튼 클릭함");
+                initData={{
+                    date: new Date().getTime(),
+                    emotionId: 1,
+                    content: "이전에 작성했던 일기",
                 }}
+                onSubmit={() => alert("작성 완료!")}
             />
         </div>
     );
