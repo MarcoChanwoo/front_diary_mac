7eb972bb1bf3a16c5f77daa4a9362820b499b586
Marco <cwhong8087@gmail.com>
2023-09-05 18:27:39 +0900

    6th commit

--- src/component/Editor.js
+++ src/component/Editor.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Editor.css";
 import { getFormattedDate } from "../util";
+import Button from "./Button";
 
 const Editor = ({ initData, onSubmit }) => {
     const [state, setState] = useState({
@@ -12,6 +13,12 @@ const Editor = ({ initData, onSubmit }) => {
         setState({
             ...state,
             date: e.target.value,
+        });
+    };
+    const handleChangeContent = (e) => {
+        setState({
+            ...state,
+            content: e.target.value,
         });
     };
 
@@ -32,10 +39,19 @@ const Editor = ({ initData, onSubmit }) => {
                 <h4>오늘의 감정</h4>
             </div>
             <div className="editor_section">
-                {/* 일기 */}
                 <h4>오늘의 일기</h4>
+                <div className="input_wrapper">
+                    <textarea
+                        placeholder="오늘은 어땠나요?"
+                        value={state.content}
+                        onChange={handleChangeContent}
+                    />
+                </div>
             </div>
-            <div className="editor_section">{/* 작성완료, 취소 */}</div>
+            <div className="editor_section bottom_section">
+                <Button text={"취소하기"} />
+                <Button text={"작성 완료"} type={"positive"} />
+            </div>
         </div>
     );
 };
