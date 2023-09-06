15f5a4344afb72be359d290aae50226335609b87
Marco <cwhong8087@gmail.com>
2023-09-06 11:20:25 +0900

    20th commit

--- src/component/DiaryItem.js
+++ src/component/DiaryItem.js
@@ -1,11 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import { getEmotionImgById } from "../util";
 import "./DiaryItem.css";
+import Button from "./Button";
 
 const DiaryItem = ({ id, emotionId, content, date }) => {
     const navigate = useNavigate();
     const goDetail = () => {
         navigate(`/diary/${id}`);
+    };
+
+    const goEdit = () => {
+        navigate(`/edit/${id}`);
     };
 
     return (
@@ -20,6 +25,15 @@ const DiaryItem = ({ id, emotionId, content, date }) => {
                     alt={`emotion${emotionId}`}
                     src={getEmotionImgById(emotionId)}
                 />
+            </div>
+            <div onClick={goDetail} className="info_section">
+                <div className="date_wrapper">
+                    {new Date(parseInt(date)).toLocaleDateString()}
+                </div>
+                <div className="content_wrapper">{content.slice(0, 25)}</div>
+            </div>
+            <div className="button_section">
+                <Button onClick={goEdit} text={"수정하기"} />
             </div>
         </div>
     );
