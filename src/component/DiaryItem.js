6160aa15b67986dd948f9f62c9a0b4cd7554ca79
Marco <cwhong8087@gmail.com>
2023-09-06 11:10:14 +0900

    19th commit

---
+++ src/component/DiaryItem.js
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { getEmotionImgById } from "../util";
+import "./DiaryItem.css";
+
+const DiaryItem = ({ id, emotionId, content, date }) => {
+    const navigate = useNavigate();
+    const goDetail = () => {
+        navigate(`/diary/${id}`);
+    };
+
+    return (
+        <div className="DiaryItem">
+            <div
+                onClick={goDetail}
+                className={["img_section", `img_section_${emotionId}`].join(
+                    " "
+                )}
+            >
+                <img
+                    alt={`emotion${emotionId}`}
+                    src={getEmotionImgById(emotionId)}
+                />
+            </div>
+        </div>
+    );
+};
+export default DiaryItem;
