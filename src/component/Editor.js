318a442e8317cf2a1b9c474b19eedad35d45685c
Marco <cwhong8087@gmail.com>
2023-09-06 14:53:43 +0900

    26th commit

--- src/component/Editor.js
+++ src/component/Editor.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./Editor.css";
 import { emotionList, getFormattedDate } from "../util";
 import Button from "./Button";
@@ -30,12 +30,13 @@ const Editor = ({ initData, onSubmit }) => {
     const handleOnGoBack = () => {
         navigate(-1);
     };
-    const handleChangeEmotion = (emotionId) => {
-        setState({
+    const handleChangeEmotion = useCallback((emotionId) => {
+        // 최적화 완료
+        setState((state) => ({
             ...state,
             emotionId,
-        });
-    };
+        }));
+    }, []);
     useEffect(() => {
         if (initData) {
             setState({
